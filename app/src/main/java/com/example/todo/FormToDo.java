dfa6e5d217cede626bf641862f061a8874d72dcb
Francisco Javier Mendoza <pacopancho413@gmail.com>
2019-08-08 00:43:42 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/todo/FormToDo.java
@@ -0,0 +1,59 @@
+package com.example.todo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+public class FormToDo extends DialogFragment {
+    public static final String TAG = "Nuevo pedido";
+
+
+    public static FormToDo display(FragmentManager fragmentManager) {
+        FormToDo formToDo = new FormToDo();
+        formToDo.show(fragmentManager, TAG);
+        return formToDo;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.form_to_do, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbarForm);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        toolbar.setTitle("Nueva tarea");
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+            dialog.getWindow().setWindowAnimations(R.style.FullScreenDialogStyle);
+        }
+    }
+}
